6149491114f1cab0fdb41f6057c16bc8a0ac750d
Gabriel Pichot <gabriel.pichot@gmail.com>
2015-06-06 19:25:53 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+
+from setuptools import setup
+
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+setup(
+    name='markdown-downheader',
+    version='0.1.0',
+    description='Change h1 to h2, h2 to h3... in markdown',
+    url='https://github.com/gpichot/markdown-downheader',
+    license='BSD',
+    py_modules=['mdx_downheader'],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=[ "markdown", ]
+) ,
